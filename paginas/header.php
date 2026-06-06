4cf419274ada02a265fe313f82381ac73d1bc9eb
Kaléu <heithorkaleuborges@gmail.com>
2026-06-06 16:43:45 -0300

    adicionando a pagina sobre

--- paginas/header.php
+++ paginas/header.php
@@ -43,7 +43,7 @@
                         </li>
 
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Sobre</a>
+                            <a class="nav-link" href="sobre.php">Sobre</a>
                         </li>
 
                     </ul>
