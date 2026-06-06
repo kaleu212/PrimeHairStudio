5b7587da92c7a8aef89cd7ce0d889f0c589bf6c4
Kaléu <heithorkaleuborges@gmail.com>
2026-06-06 16:46:29 -0300

    index.php#servicos

--- paginas/header.php
+++ paginas/header.php
@@ -35,7 +35,7 @@
                         </li>
 
                         <li class="nav-item">
-                            <a class="nav-link" href="#servicos">Serviços</a>
+                            <a class="nav-link" href="index.php#servicos">Serviços</a>
                         </li>
 
                         <li class="nav-item">
